d57d9db2e14ce834ee772730440f2ff4080fa518
Facundo <salasfacundo96@gmail.com>
2024-10-19 11:42:16 -0300

    estructura inicial

---
+++ src/app/dashboard/pages/dashboard-page/dashboard-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dashboard-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-page.component.html',
+  styleUrl: './dashboard-page.component.scss'
+})
+export class DashboardPageComponent {
+
+}
