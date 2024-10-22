aa61ec538e042c342faf1d48574eabdaec9989c8
Facundo <salasfacundo96@gmail.com>
2024-10-21 21:09:39 -0300

    El filtro y acciones no mostraran modal si no existen todos

--- src/app/dashboard/components/todo-filter/todo-filter.component.ts
+++ src/app/dashboard/components/todo-filter/todo-filter.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TodoService } from '../../services/todo.service';
 import { FilterEnum } from '../../enums/filterEnum.enum';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmationModalComponent } from '../../modals/confirmation-modal/confirmation-modal.component';
 import { NotificationService } from '../../services/notification.service';
+import { Todo } from '../../models/Todo';
 
 @Component({
   selector: 'todo-filter',
@@ -12,14 +13,22 @@ import { NotificationService } from '../../services/notification.service';
   templateUrl: './todo-filter.component.html',
   styleUrl: './todo-filter.component.scss'
 })
-export class TodoFilterComponent {
+export class TodoFilterComponent implements OnInit{
+
+  list: Todo[] = [];
 
   constructor(private todoService: TodoService, private dialog: MatDialog, private notificationService: NotificationService){}
+
+  ngOnInit(){
+    this.todoService.getAllTodos().subscribe(response => {
+      this.list = response;
+    })
+  }
 
   filter(option: string){
     const filterOption = FilterEnum[option.toUpperCase() as keyof typeof FilterEnum];
 
-    if (filterOption) {
+    if (filterOption && this.list.length > 0) {
       this.todoService.filterTodos(filterOption);
     } else {
       console.error('Invalid filter option');
@@ -27,13 +36,15 @@ export class TodoFilterComponent {
   }
 
   openModal(action: string){
-    const dialogRef = this.dialog.open(ConfirmationModalComponent);
+    if(this.list.length > 0){
+      const dialogRef = this.dialog.open(ConfirmationModalComponent);
 
     dialogRef.afterClosed().subscribe((result: any) => {
       if(result){
         this.action(action);
       }
     });
+    }
   }
 
   action(action: string){
