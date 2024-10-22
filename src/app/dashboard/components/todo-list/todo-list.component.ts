69a918993ccab7bfc93541c90bba2cb1b4798964
Facundo <salasfacundo96@gmail.com>
2024-10-22 00:26:16 -0300

    Filtro se elimino y se agrego al final de la lista

--- src/app/dashboard/components/todo-list/todo-list.component.ts
+++ src/app/dashboard/components/todo-list/todo-list.component.ts
@@ -4,6 +4,10 @@ import { CommonModule } from '@angular/common';
 import { TodoCardComponent } from '../todo-card/todo-card.component';
 import { TodoService } from '../../services/todo.service';
 import { Todo } from '../../models/Todo';
+import { NotificationService } from '../../services/notification.service';
+import { ConfirmationModalComponent } from '../../modals/confirmation-modal/confirmation-modal.component';
+import { MatDialog } from '@angular/material/dialog';
+import { FilterEnum } from '../../enums/filterEnum.enum';
 
 @Component({
   selector: 'todo-list',
@@ -15,8 +19,9 @@ import { Todo } from '../../models/Todo';
 export class TodoListComponent implements OnInit{
 
   list!: Todo[];
+  selectedFilter: string = "all"
 
-  constructor(private todoService: TodoService){}
+  constructor(private todoService: TodoService, private notificationService: NotificationService, private dialog: MatDialog){}
 
   ngOnInit(): void {
     this.todoService.getAllTodos().subscribe( response => {
@@ -28,4 +33,42 @@ export class TodoListComponent implements OnInit{
     moveItemInArray(this.list, event.previousIndex, event.currentIndex);
     this.todoService.updateTodoOrder(this.list);
   }
+
+  openModal(action: string){
+    if(this.list.length > 0){
+      const dialogRef = this.dialog.open(ConfirmationModalComponent);
+
+    dialogRef.afterClosed().subscribe((result: any) => {
+      if(result){
+        this.action(action);
+      }
+    });
+    }
+  }
+
+  action(action: string){
+    switch (action) {
+      case 'deleteAll':
+        this.todoService.deleteAll();
+        this.notificationService.showNotification('All todos were deleted');
+        break;
+      case 'deleteCompleted':
+        this.todoService.clearCompleted();
+        this.notificationService.showNotification('Completed todos were deleted');
+        break;
+    }
+  }
+
+  filter(option: string){
+    this.selectedFilter = option;
+    console.log(this.selectedFilter)
+    const filterOption = FilterEnum[option.toUpperCase() as keyof typeof FilterEnum];
+
+    if (filterOption && this.list.length > 0) {
+      this.todoService.filterTodos(filterOption);
+    } else {
+      console.error('Invalid filter option');
+    }
+  }
+
 }
--- src/app/dashboard/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/dashboard/pages/dashboard-page/dashboard-page.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { TodoListComponent } from '../../components/todo-list/todo-list.component';
-import { TodoFilterComponent } from '../../components/todo-filter/todo-filter.component';
 import { MatIconModule } from '@angular/material/icon';
 import { NewTodoComponent } from '../../components/new-todo/todo-new.component';
 
 @Component({
   selector: 'dashboard-page',
   standalone: true,
-  imports: [TodoListComponent, TodoFilterComponent, NewTodoComponent, MatIconModule],
+  imports: [TodoListComponent, NewTodoComponent, MatIconModule],
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.scss'
 })
